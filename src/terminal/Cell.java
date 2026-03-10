107e6135a171a284b614479c36ac997eea62e1e8
xGogax <lacastanko03@gmail.com>
2026-03-10 09:35:11 +0100

    Override toString to be able to System.out.println(cell).
    added Test for Cell

--- src/terminal/Cell.java
+++ src/terminal/Cell.java
@@ -9,6 +9,23 @@ public class Cell {
 
     public Cell(char character) { this.character = character; }
 
+    @Override
+    public String toString() {
+        int fg = foregroundColor.getAnsiForeground();
+        int bg = backgroundColor.getAnsiBackground();
+
+        // styleCodeFormat example: "1;4;3" for BOLD, UNDERLINE, ITALIC
+        StringBuilder styleCode = new StringBuilder();
+        for (int i = 0; i < styleFlags.length; i++) {
+            styleCode.append(styleFlags[i].getCode());
+            if (i < styleFlags.length - 1) styleCode.append(";");
+        }
+
+        String style = !styleCode.isEmpty() ? styleCode.toString() + ";" : "";
+
+        return "\u001B[" + style + fg + ";" + bg + "m" + character + "\u001B[0m";
+    }
+
     // <editor-fold defaultstate="collapsed" desc="Getters and Setters">
     public char getCharacter() { return character; }
     public void setCharacter(char character) { this.character = character; }
---
+++ src/test/TestCell.java
@@ -0,0 +1,59 @@
+package test;
+
+import terminal.Cell;
+import terminal.Color;
+import terminal.StyleFlag;
+
+public class TestCell {
+    public static void main(String[] args) {
+        System.out.println("-------------------------------------");
+        System.out.println("           NORMAL CELL TEST");
+        System.out.println("-------------------------------------");
+
+        Cell cell1 = new Cell('A');
+        Cell cell2 = new Cell('B');
+        Cell cell3 = new Cell(' ');
+        Cell cell4 = new Cell('C');
+
+        // Only StyleFlags test
+        cell1.setStyleFlags(StyleFlag.BOLD, StyleFlag.UNDERLINE);
+
+        // Only FG and BG test
+        cell2.setForegroundColor(terminal.Color.BRIGHT_GREEN);
+        cell2.setBackgroundColor(terminal.Color.GREEN);
+
+        // All combined test
+        cell3.setForegroundColor(terminal.Color.WHITE);
+        cell3.setBackgroundColor(terminal.Color.BRIGHT_MAGENTA);
+        cell3.setStyleFlags(StyleFlag.ITALIC);
+
+        // Only FG and StyleFlags test
+        cell4.setForegroundColor(terminal.Color.CYAN);
+        cell4.setStyleFlags(StyleFlag.STRIKETHROUGH, StyleFlag.BOLD, StyleFlag.ITALIC, StyleFlag.UNDERLINE);
+
+        // output
+        System.out.println(cell1);
+        System.out.println(cell2);
+        System.out.println(cell3);
+        System.out.println(cell4);
+
+        System.out.println("-------------------------------------");
+        System.out.println("    EVERY COLOR COMBINATION TEST");
+        System.out.println("-------------------------------------");
+
+        int i = 1;
+        Cell cell = new Cell('X');
+        for(Color backgroundColor : Color.values()){
+            for(Color foregroundColor : Color.values()){
+                cell.setBackgroundColor(backgroundColor);
+                cell.setForegroundColor(foregroundColor);
+                if(i % 4 == 0){
+                    System.out.println(cell);
+                } else {
+                    System.out.print(cell);
+                }
+                i++;
+            }
+        }
+    }
+}
